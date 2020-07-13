0eababc7d22288a5a689d20f94f58d2217f13018
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-13 21:06:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ function sayHiToGrandma(string) {
   var uppercase = 'HELLO!';
   var mixedCase = 'Hi there!';
   
- if ( 'hello!'.toLowerCase() === lowercase ) {
+ if ( string === lowercase ) {
     console.log(`I can't hear you!`);
-  } if ('HELLO!'.toUpperCase() === uppercase){
-    console.log("YES INDEED!");
-  }  if ('I love you, Grandma'.mixedCase === "I love you, Grandma.") {
-    console.log("I love you, too.");
-  }
-
+  } 
 }
